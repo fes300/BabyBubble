973fd5aef59c6ce12bc6c5b547386e8a5d7b1b06
fes300 <federicosordillo@gmail.com>
2016-03-28 13:33:44 +0200

    imporovements

--- web/js/myJs.js
+++ web/js/myJs.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     $overlay = $('<div id="overlay"></div>');
     $("body").append($overlay);
 
+    $('#body-cover').height($(window).height());
+
     $('#overlay').click(function(){
         $(this).hide();
         $('#newAppointment').hide();
